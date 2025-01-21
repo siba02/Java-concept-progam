0a3d0bb5490d657b54df01d2bf156b2f82ef148a
siba02 <sibaprasad.tofan@gmail.com>
2025-01-21 15:19:56 +0530

    update

---
+++ forloopcheck.java
@@ -0,0 +1,32 @@
+public class forloopcheck {
+    public static void main(String[] args) {
+
+// for (int i = 0; true; i++)
+//     System.out.println("Hello");          //Hello........
+
+// int i = 0;
+// for(System.out.println("Hi");i<3;i++){      //Hi Hello Hello Hello
+//     System.out.println("Hello");
+// }
+
+
+// int i = 0;
+// for(System.out.println("Hi"); ;i++){      //Hi Hello........
+//     System.out.println("Hello");
+// }
+
+int i = 0;
+for(System.out.println("Hi");i<3;System.out.println("Hello")){      //Hi Hello Hello Hello
+    i++;
+}
+
+
+
+
+
+
+        
+    
+    
+}
+}
