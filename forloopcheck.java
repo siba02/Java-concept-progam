ff3052fa612310151e9d4ca9cb2da9006eff43b8
siba02 <sibaprasad.tofan@gmail.com>
2025-01-21 16:00:28 +0530

    update

--- forloopcheck.java
+++ forloopcheck.java
@@ -1,5 +1,8 @@
 public class forloopcheck {
     public static void main(String[] args) {
+   
+        
+
 
 // for (int i = 0; true; i++)
 //     System.out.println("Hello");          //Hello........
@@ -15,18 +18,52 @@ public class forloopcheck {
 //     System.out.println("Hello");
 // }
 
-int i = 0;
-for(System.out.println("Hi");i<3;System.out.println("Hello")){      //Hi Hello Hello Hello
-    i++;
+// int i = 0;
+// for(System.out.println("Hi");i<3;System.out.println("Hello")){      //Hi Hello Hello Hello
+//     i++;
+// }
+
+
+// for(int i =0;true;i++){
+//     System.out.println("Hello");
+// }
+//  System.out.println("Hi");                 //Unreachable statement.
+
+
+    // for(int i =0;false;i++){
+    //     System.out.println("Hello");           //Unreachable statement.
+    // }
+    //  System.out.println("Hi");                 
+    
+    // int a=10,b=20;
+// for(int i =0;a<b;i++){
+//     System.out.println("Hello");           //Hello .........
+// }
+//  System.out.println("Hi");                 
+
+int a=10,b=20;
+for(int i =0;a>b;i++){
+    System.out.println("Hello");           //Hi
 }
+ System.out.println("Hi");                 
 
 
 
+//  final int a=10,b=20;
+// for(int i =0;a>b;i++){
+//     System.out.println("Hello");           //Unrechable statement
+// }
+//  System.out.println("Hi");                 
 
+// final int a=10,b=20;
+// for(int i =0;a<b;i++){
+//     System.out.println("Hello");          
+// }
+//  System.out.println("Hi");                  //Unrechable statement
 
 
         
     
     
-}
+   }
 }
