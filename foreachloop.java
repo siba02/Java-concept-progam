6da7905dcd3a85b12a49957b579d13617c465885
siba02 <sibaprasad.tofan@gmail.com>
2025-01-23 18:10:49 +0530

    new file

--- foreachloop.java
+++ foreachloop.java
@@ -1,6 +1,7 @@
 public class foreachloop {
     public static void main(String[] args) {
-        int [][] x={{10,20,30},{40,50}};
+        // int [][] x={{10,20,30},{40,50}};
+        int[][][]x={{{10,20},{30,40}},{{50,60,70},{80,90}}};
 
         // for(int i=0; i<x.length; i++) {
         //     for(int j=0; j<x[i].length; j++) {
@@ -8,11 +9,19 @@ public class foreachloop {
         //     }
         // }
 
-        for(int[]x1:x){
-            for(int x2:x1){
-                System.out.print(x2+" ");
+        // for(int[]x1:x){
+        //     for(int x2:x1){
+        //         System.out.print(x2+" ");
+        //     }
+        // }
+
+
+        for(int[][]x1:x){
+            for(int[]x2:x1){
+                for(int x3:x2){
+                    System.out.print(x3+" ");
+                }
             }
         }
-
   }
 }
---
+++ sourcefile.java
@@ -0,0 +1,11 @@
+//  we can define any no of classes in a single source file.
+// if there is any public class then the file name must be public class name.
+// the no of classes is defined in a source file that many.class file is generated once it is compiled.
+ 
+ class sourcefile {                           //we can give the file name as any if there is no public class.like(sourcefile).
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        
+    }
+    
+}
