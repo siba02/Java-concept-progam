01e5fb38f0576cbd9e9015b3e047a78980306495
siba02 <sibaprasad.tofan@gmail.com>
2025-01-23 18:23:13 +0530

    update

--- sourcefile.java
+++ sourcefile.java
@@ -2,10 +2,17 @@
 // if there is any public class then the file name must be public class name.
 // the no of classes is defined in a source file that many.class file is generated once it is compiled.
  
- class sourcefile {                           //we can give the file name as any if there is no public class.like(sourcefile).
+  public class sourcefile {   
+                       //we can give the file name as any if there is no public class.like(sourcefile).
     public static void main(String[] args) {
         System.out.println("Hello, world!");
         
     }
+    class A{
+      
+    }     
+  
+        
+    
     
 }
