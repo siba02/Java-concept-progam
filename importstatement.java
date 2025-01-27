d42ff7452601b0b2752c64ff4a2a6de346249c6b
siba02 <sibaprasad.tofan@gmail.com>
2025-01-27 23:14:13 +0530

    update

---
+++ importstatement.java
@@ -0,0 +1,21 @@
+
+import java.util.ArrayList;    //this is explict import.
+// import java.lang.Syatem;   //it is explictly availabe in   java no need to mention it.
+// import a.*;    //compiler understand whatever the classes inside the a package only. 
+// package a.b.c.d.Test;
+// Explict class import got highest priority.
+// Implict import not recomended.
+// Explict import  recomended.
+// Always explict import is recomended for easy redability.
+// System is available inside the lang package and is available explicitly no need to mention.
+
+
+
+
+public class importstatement {
+    public static void main(String[] args) {
+        ArrayList ab=new ArrayList();
+        System.out.println("ab");
+    }
+    
+}
--- sourcefile.java
+++ sourcefile.java
@@ -1,6 +1,8 @@
 //  we can define any no of classes in a single source file.
 // if there is any public class then the file name must be public class name.
 // the no of classes is defined in a source file that many.class file is generated once it is compiled.
+// not recomended to write multiple classes in a single source file.
+
  
   public class sourcefile {   
                        //we can give the file name as any if there is no public class.like(sourcefile).
