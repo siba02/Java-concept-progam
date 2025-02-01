357900ebc502e29fee2cafa0e99391b418a5d695
siba02 <sibaprasad.tofan@gmail.com>
2025-02-01 13:57:42 +0530

    update

---
+++ pack1/X.java
@@ -0,0 +1,8 @@
+package pack1;
+
+public class X {
+    protected void showMessage() {
+        System.out.println("Protected Method in Class A");
+    }
+}
+
---
+++ pack1/Y.java
@@ -0,0 +1,11 @@
+package pack1;
+public class Y {
+
+    public static void main(String[] args) {
+        X obj = new X();
+        obj.showMessage();  // ✅ Allowed: Same package
+    }
+}
+
+    
+
