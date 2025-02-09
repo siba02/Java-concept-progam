149f5f1aa9e04eebbded0c8f0b2801476dcdd474
siba02 <sibaprasad.tofan@gmail.com>
2025-02-09 23:44:34 +0530

    updates

--- StringExamp2.java
+++ StringExamp2.java
@@ -1,10 +1,10 @@
 public class StringExamp2 {
 
     public static void main(String[] args) {
-        // String s1=new String("Hello");
-        // String s2=new String("Hello");
-        // System.out.println(s1==s2);      // (false) the == operator always check for the reference comparison.
-        // System.out.println(s1.equals(s1));   // (true) in string the equals method is overridden.
+        String s1=new String("Hello");
+        String s2=new String("Hello");
+        System.out.println(s1==s2);      // (false) the == operator always check for the reference comparison.
+        System.out.println(s1.equals(s1));   // (true) in string the equals method is overridden.
 
         StringBuffer sb1=new StringBuffer("Hello");
         StringBuffer sb2=new StringBuffer("Hello");
