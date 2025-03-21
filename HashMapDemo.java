8fd0a6b69bf2bfab38f21713d97fa1369a4d53d4
siba02 <sibaprasad.tofan@gmail.com>
2025-03-21 15:45:30 +0530

    updates

---
+++ HashMapDemo.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class HashMapDemo {
+    public static void main(String[] args) {
+        HashMap m = new HashMap();
+        m.put("siba",100);
+        m.put("tofan",800);
+        m.put("talcher",900);
+        m.put("manas",500);
+        System.out.println(m);  //{siba=100, manas=500, talcher=900, tofan=800}
+        System.out.println(m.put("siba",400)); //100
+
+        Set s=m.keySet();
+        System.out.println(s);//[siba, manas, talcher, tofan]
+
+        Collection c=m.values();
+        System.out.println(c); //[400, 500, 900, 800]
+
+        Set s1=m.entrySet();
+        System.out.println(s1); //[siba=400, manas=500, talcher=900, tofan=800]
+
+        Iterator it=s1.iterator();
+        while(it.hasNext()){
+            Map.Entry m1=(Map.Entry)it.next();
+            System.out.println(m1.getKey() +"-->"+ m1.getValue());
+
+            if(m1.getKey().equals("siba")){
+                m1.setValue(500);
+            }
+        }
+        System.out.println(m); //{siba=500, manas=500, talcher=900, tofan=800}
+
+        
+    }
+    
+}
